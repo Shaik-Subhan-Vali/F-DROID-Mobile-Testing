c3a182334384bb8149a6886926861f45702cfea5
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-29 17:15:29 +0530

    Tests added

--- src/test/java/F_Droid_Automation.java
+++ src/test/java/F_Droid_Automation.java
@@ -8,6 +8,8 @@ import Native_apps.F_Droid_Capabilities;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class F_Droid_Automation extends F_Droid_Capabilities {
 	
@@ -19,11 +21,35 @@ AndroidDriver<AndroidElement> driver ;
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
-	@Test(enabled = false)
-	public void preferencesettings() throws InterruptedException {
+	@Test
+	public void possible_actions() throws InterruptedException {
 		
-		driver.findElement(MobileBy.AccessibilityId("Preference")).click();
+		driver.findElement(MobileBy.id("org.fdroid.fdroid:id/settings")).click();
+		driver.findElement(MobileBy.id("org.fdroid.fdroid:id/switchWidget")).click();
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+	}
+	@Test
+	public void batterysaver(){
 		
+		driver.openNotifications();
+		driver.findElements(MobileBy.id("android:id/icon")).get(3).click();
+		driver.findElement(MobileBy.id("com.android.systemui:id/fake_shadow")).click();
+		
+	
+	}
+	@Test
+	public void switchingapps() {		
+		
+		driver.activateApp("com.google.android.apps.messaging");
+		
+		
+		String exp = driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"You: hmmmmm...!\")")).getText();
+		System.out.println(exp);
+			
+		driver.activateApp("org.fdroid.fdroid");
+		
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+			
 	}
 
 }
